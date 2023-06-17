59cd79303ce345c6e222768da44bbecccfd97368
dennisil <dennisilye@gmail.com>
2023-06-17 21:45:15 +0300

    feat: sidebar

---
+++ components/Sidebar/Left.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="h-screen flex flex-col">
+    <div class="p-2 my-2 hover:bg-blue-50 rounded-full w-min
+    dark:hover:bg-white/20" :class="defaultTransition">
+      <nuxt-link to="/">
+        <div class="w-8 h-8">
+          <LogoTwitter />
+        </div>
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import useTailwindConfig from '../../composables/useTailwindConfig';
+
+const { defaultTransition } = useTailwindConfig()
+
+</script>
